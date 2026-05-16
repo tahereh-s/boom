01d69401d2475f82bc781687fbe2ced123347944
Tahere Salemi <Salemi@fava.net>
2026-05-16 07:58:56 +0330

    install shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans ">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white  sm:items-start">
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -58,6 +59,7 @@ export default function Home() {
           >
             Documentation
           </a>
+          <Button variant="secondary"  >shadcn</Button>
         </div>
       </main>
     </div>
